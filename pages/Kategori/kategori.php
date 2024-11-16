826ef4f8c1ce74df3dbecee278602ee7ba8acc1b
har-neptune <muhtria.har@gmail.com>
2024-11-16 16:39:47 +0700

    update

--- pages/Kategori/kategori.php
+++ pages/Kategori/kategori.php
@@ -76,10 +76,46 @@ oci_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Kategori</title>
+    <link rel="shortcut icon" href="../../public/img/icon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
+    <!-- Header Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light navbar-container">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../../public/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                <span class="navbar-title">Pemay</span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="users.php">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../Stock/stock.php">Stock</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="crud_kategori.php">Kategori</a>
+                    </li>
+                </ul>
+            </div>
+
+            <form action="../../auth/logout.php" method="post">
+                <button class="btn btn-link text-dark text-decoration-none" type="submit">Logout</button>
+            </form>
+        </div>
+    </nav>
+
+    <!-- Content -->
     <div class="container mt-5">
         <h2>CRUD Kategori</h2>
         <?php if (isset($message)): ?>
@@ -145,6 +181,10 @@ oci_close($conn);
             </table>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
