0c0edfc53e937030589881b2185ad1099d339ec9
har-neptune <muhtria.har@gmail.com>
2024-11-16 19:11:59 +0700

    update

--- pages/Stock/add_stock.php
+++ pages/Stock/add_stock.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 include '../../config/connection.php';
+include '../owner/header.php';
 
 // Check if the user is logged in and the session is active
 if (!isset($_SESSION['pegawai_id'])) {
-    // If no session exists, redirect to the login page
     header("Location: ../../login.php");
     exit();
 }
@@ -50,52 +50,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 oci_close($conn);
 ?>
-
+<body>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Stock Item</title>
-    <link rel="shortcut icon" href="../../public/img/icon.png" type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light navbar-container">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="../../public/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
-                <span class="navbar-title">Pemay</span>
-            </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/pemay/pages/owner/dashboard.php">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/pemay/pages/owner/users.php">Users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/pemay/pages/Stock/stock.php">Stock</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/pemay/pages/Kategori/kategori.php">Kategori</a>
-                    </li>
-                </ul>
-            </div>
-
-            <form action="../../auth/logout.php" method="post">
-                <button class="btn btn-link text-dark text-decoration-none" type="submit">Logout</button>
-            </form>
-        </div>
-    </nav>
-
     <div class="container mt-4">
         <h2>Add Stock Item</h2>
         <form action="add_stock.php" method="post">
