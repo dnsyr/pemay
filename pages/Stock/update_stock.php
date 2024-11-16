5016794c2479c5973c9216f1c184c248c1f0f460
har-neptune <muhtria.har@gmail.com>
2024-11-16 14:30:49 +0700

    update

--- pages/Stock/update_stock.php
+++ pages/Stock/update_stock.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include '../../config/connection.php';
-include '../../pages/owner/dashboard.php';
+
 // Fetch stock item for editing
 $id = $_GET['id'] ?? null;
 $stock = null;
@@ -50,10 +50,7 @@ oci_close($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Stock Item</title>
     <link rel="shortcut icon" href="../../public/img/icon.png" type="image/x-icon">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
     <link rel="stylesheet" href="../../public/css/index.css">
 </head>
 
@@ -64,14 +61,10 @@ oci_close($conn);
                 <img src="../../public/img/icon.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
                 <span class="navbar-title">Pemay</span>
             </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="dashboard.php">Dashboard</a>
+                        <a class="nav-link" href="dashboard.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="users.php">Users</a>
@@ -79,15 +72,8 @@ oci_close($conn);
                     <li class="nav-item">
                         <a class="nav-link active" href="stock.php">Stock</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
                 </ul>
             </div>
-
-            <form action="../../auth/logout.php" method="post">
-                <button class="btn btn-link text-dark text-decoration-none" type="submit">Logout</button>
-            </form>
         </div>
     </nav>
 
@@ -99,7 +85,7 @@ oci_close($conn);
                     <label for="nama_item" class="form-label">Item Name</label>
                     <input type="text" class="form-control" id="nama_item" name="nama_item" value="<?php echo htmlentities($stock['NAMAITEM']); ?>" required>
                 </div>
-                <div class="mb- 3">
+                <div class="mb-3">
                     <label for="jumlah" class="form-label">Quantity</label>
                     <input type="number" class="form-control" id="jumlah" name="jumlah" value="<?php echo htmlentities($stock['JUMLAH']); ?>" required>
                 </div>
@@ -123,7 +109,7 @@ oci_close($conn);
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
