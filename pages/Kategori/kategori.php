3a48b1101b6bb032dc83c88bf861996a96f0dfc6
har-neptune <muhtria.har@gmail.com>
2024-11-16 16:53:49 +0700

    update

--- pages/Kategori/kategori.php
+++ pages/Kategori/kategori.php
@@ -73,11 +73,15 @@ oci_close($conn);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Kategori</title>
-    <link rel="shortcut icon" href="../../public/img/icon.png" type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Manage Users</title>
+  <link rel="shortcut icon" href="../../public/img/icon.png" type="image/x-icon">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+  <link rel="stylesheet" href="../../public/css/index.css">
 </head>
 
 <body>
