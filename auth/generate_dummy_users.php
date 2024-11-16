3a9a4ed36b4896d8b123219b40c09433627e395c
har-neptune <muhtria.har@gmail.com>
2024-11-16 16:27:58 +0700

    update

--- auth/generate_dummy_users.php
+++ auth/generate_dummy_users.php
@@ -22,7 +22,7 @@ function insertDummyUser($db, $name, $username, $password, $position, $email, $p
 }
 
 try {
-  $db = new PDO("oci:dbname=//localhost/XE", "DVF", "DVF");
+  $db = new PDO("oci:dbname=//localhost/XE", "C##PETSHOP", "petshop");
 
   insertDummyUser($db, 'Owner User', 'owner', 'owner', 'owner', 'owner@example.com', '1234567890');
   insertDummyUser($db, 'Staff User', 'staff', 'staff', 'staff', 'staff@example.com', '1234567120');
